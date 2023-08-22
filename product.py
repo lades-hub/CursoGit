01c59f5a859fa4b3c05c7a43695ef51d804fee88
LADES <sergio@peq.coppe.ufrj.br>
2023-08-22 09:28:01 -0300

    adicine lineas sobre las anteriores

--- product.py
+++ product.py
@@ -5,6 +5,8 @@ def product(a, b):
 
 if __name__ == '__main__':
     print(product(2, 3))
-    print(product(-2, 9))
-    print(product(-4, 6))
+    g = 5 * 8
+    h = 8 * 5
+    print("Productos locos")
     print(product(8, 73))
+    print("Fin del codigo")
